1506d43ac341a446cd44bdbb7a369a8eff9511ac
Tomer Solomon <tomer.solomon1@gmail.com>
2017-08-09 19:58:09 +0300

    updating board

--- Board.c
+++ Board.c
@@ -9,7 +9,6 @@
 #include <Board.h>
 #include <stdbool.h>
 
-
 Board *create_board() {
 	return NULL;
 }
@@ -30,6 +29,37 @@ bool is_valid_move(Board *board, Move move) {
 	return true;
 }
 
-int **get_all_moves(Board *board, int colur) {
+bool is_check_curr_player(Board *board) {
+	return false;
+}
+
+Move *get_moves(Board *board, Piece *piece) {
 	return NULL;
+}
+
+Move *get_all_moves(Board *board, int colur) {
+	return NULL;
+}
+
+Piece *get_piece_in_place(Board *board, int row, int col) {
+	return NULL;
+}
+
+bool is_undo_valid(Board *board) {
+	return true;
+}
+
+SP_CHESS_BOARD_MESSAGE undo_move(Board *board) {
+	return SP_CHESS_BOARD_SUCCESS;
+}
+
+char is_game_over(Board *board) {
+	return 'a';
+}
+
+bool is_check_mate(Board *board) {
+	return true;
+}
+
+void print_board(Board *board) {
 }
