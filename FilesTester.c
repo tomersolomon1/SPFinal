0f59a16d70bd6801538dd91d21456796d4235971
sapkode <sapir@172.17.206.222>
2017-08-16 20:13:18 +0300

    fileteseter

---
+++ FilesTester.c
@@ -0,0 +1,37 @@
+/*
+ * FilesTester.c
+ *
+ *  Created on: Aug 16, 2017
+ *      Author: sapir
+ */
+
+#include "FilesTester.h"
+void check_Files(){
+	Gameboard *game = create_board(0,0,0);
+
+	set_step(game, 1,1,2,1);
+	set_step(game, 6,1,5,1);
+	set_step(game, 0,1,2,2);
+	set_step(game, 6,2,4,2);
+	undo_step(game);
+	set_step(game, 6,3,4,3);
+    set_step(game, 2,2,4,3);
+	set_step(game, 7,3,4,3);
+	set_step(game, 1,0,2,0);
+	//set_step(game, 4,3,1,3);
+	//set_step(game, 0,5,1,3);
+	print_details_game(game);
+	//write:
+	FILE *fw = fopen(my_path, "w");
+	save_xml(fw,game);
+	fclose(fw);
+	destroy_board(game);
+	//read:
+	Gameboard * game2 = NULL;
+	FILE *fr = fopen(my_path, "r");
+
+	game2 = load_game(fr);
+	fclose(fr);
+	print_details_game(game2);
+
+}
---
+++ FilesTester.h
@@ -0,0 +1,15 @@
+/*
+ * FileTester.h
+ *
+ *  Created on: Aug 16, 2017
+ *      Author: sapir
+ */
+
+#ifndef FILESTESTER_H_
+#define FILESTESTER_H_
+#include "Files.h"
+#include <stdio.h>
+#define my_path "C:\\Users\\sapir\\Desktop\\try.txt"
+
+void check_Files();
+#endif /* FILESTESTER_H_ */
