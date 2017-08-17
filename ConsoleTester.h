e7b7afc06098b2a806e2aea59824698e18c17887
Tomer Solomon <tomer.solomon1@gmail.com>
2017-08-17 22:43:59 +0300

    Test.c

---
+++ ConsoleTester.h
@@ -0,0 +1,14 @@
+/*
+ * ConsoleTester.h
+ *
+ *  Created on: 17 баев„ 2017
+ *      Author: User
+ */
+
+#ifndef CONSOLETESTER_H_
+#define CONSOLETESTER_H_
+
+void console_tester();
+void test_strings();
+
+#endif /* CONSOLETESTER_H_ */
--- Test.c
+++ Test.c
@@ -9,11 +9,13 @@
 #include "GameBoardTester.h"
 #include "ParserTester.h"
 #include "FilesTester.h"
+#include "ConsoleTester.h"
 
 int main(int argc, char *argv[]){
 	if (argc == 2) {
 		if (strcmp(argv[1], "0") == 0) { /* Tomer's tests  */
-			check_parser();
+			//check_parser();
+			console_tester();
 		} else if (strcmp(argv[1], "1") == 0) { /* Sapir's tests */
 			check_GameBoard();
 		}
