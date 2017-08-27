0617e4804cb8f9a70d3f8147857b7a24b47b4460
sapkode <sapir@172.16.68.26>
2017-08-27 21:26:56 +0300

    default

--- graphics/Menu_windows.c
+++ graphics/Menu_windows.c
@@ -14,7 +14,7 @@ MenuWindow* create_menu_window(menu_window_type type){
 	src->type = type;
 
 	SDL_Window* window = SDL_CreateWindow(window_name[type], SDL_WINDOWPOS_CENTERED,
-			SDL_WINDOWPOS_CENTERED, 700, 700, SDL_WINDOW_OPENGL);
+			SDL_WINDOWPOS_CENTERED, DEFAULT_MENU_WINDOW_WIDTH, DEFAULT_MENU_WINDOW_HIGHT, SDL_WINDOW_OPENGL);
 	if(window == NULL) return NULL;
 	src->window = window;
 
@@ -60,11 +60,10 @@ Button** crate_enterance_buttons(SDL_Renderer* renderer){
 Button** crate_enterance_buttons(SDL_Renderer* renderer){
 	Button** buttons = malloc(sizeof(Button*) * 3);
 	if(buttons == NULL) return NULL;
-	int gap_window = 100;
 	int i = 0;
-	SDL_Rect new_game = {.x = 75, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
-	SDL_Rect load = {.x = 75, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
-	SDL_Rect exit = {.x = 75, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect new_game = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect load = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect exit = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
 
 	buttons[0] = createButton(renderer, new_game, "./images/new_game.bmp",
 			"./images/new_game_inactive.bmp", NewGameButton, true, true);
@@ -83,7 +82,30 @@ Button** crate_enterance_buttons(SDL_Renderer* renderer){
 }
 
 Button** create_load_game_buttons(SDL_Renderer* renderer){
+	Button** buttons = malloc(sizeof(Button*) * 3);
+	if(buttons == NULL) return NULL;
+	int i = 0;
+	SDL_Rect slot1 = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect slot2 = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect slot3 = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect slot4 = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect slot5 = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(i++), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
+	SDL_Rect back = {.x = DEFAULT_GAP_WINDOW, .y = DEFAULT_GAP_WINDOW + DEFAULT_BTN_GAP_VERTICAL*(3*i), .h = DEFAULT_BTN_HIGHT, .w = DEFAULT_BTN_WIDTH};
 
+	buttons[0] = createButton(renderer, new_game, "./images/new_game.bmp",
+			"./images/new_game_inactive.bmp", NewGameButton, true, true);
+	buttons[1] = createButton(renderer, load, "./images/load.bmp",
+				"./images/load_inactive.bmp", LoadButton, true, true);
+	buttons[2] = createButton(renderer, exit, "./images/exit.bmp",
+					"./images/exit_inactive.bmp", ExitButton, true, true);
+	if(buttons[0] == NULL || buttons[1] == NULL || buttons[2] == NULL ){
+		destroyButton(buttons[0]);
+		destroyButton(buttons[1]);
+		destroyButton(buttons[2]);
+		free(buttons);
+		return NULL;
+	}
+	return buttons;
 }
 
 Button** create_game_mode_buttons(SDL_Renderer* renderer){}
