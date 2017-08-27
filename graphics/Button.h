852560e1cf59f89c2082c14f8e80814fe38bb1cf
Tomer Solomon <tomer.solomon1@gmail.com>
2017-08-27 12:33:23 +0300

    button & widget

---
+++ graphics/Button.h
@@ -0,0 +1,13 @@
+/*
+ * Button.h
+ *
+ *  Created on: 27 баев„ 2017
+ *      Author: User
+ */
+
+#ifndef GRAPHICS_BUTTON_H_
+#define GRAPHICS_BUTTON_H_
+
+
+
+#endif /* GRAPHICS_BUTTON_H_ */
---
+++ graphics/Widget.h
@@ -0,0 +1,13 @@
+/*
+ * Widget.h
+ *
+ *  Created on: 27 баев„ 2017
+ *      Author: User
+ */
+
+#ifndef GRAPHICS_WIDGET_H_
+#define GRAPHICS_WIDGET_H_
+
+
+
+#endif /* GRAPHICS_WIDGET_H_ */
