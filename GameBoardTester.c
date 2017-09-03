9dba93f574ff33b39618159aaa6778bc29f159c8
sapkode <sapir@LAPTOP-1F0TI8S8>
2017-09-03 23:02:19 +0300

    cds

--- GameBoardTester.c
+++ GameBoardTester.c
@@ -11,7 +11,11 @@
 
 
 void check_GameBoard() {
-	Gameboard * game = create_board(3,3,white);
+	FILE *f = fopen("C:\\Users\\sapir\\Desktop\\save0.xml", "r");
+	assert(f != NULL);
+	Gameboard * game = load_game(f);
+	fclose(f);
+	print_details_game(game);
 //hazraha:
 /*	set_step(game, 1,1,2,1);
 	set_step(game, 7,1,5,0);
@@ -30,14 +34,14 @@ void check_GameBoard() {
 	set_step(game, 3,3,1,4);
 */
 //check:
-	set_step(game, 1,3,2,3);
+/*	set_step(game, 1,3,2,3);
 	set_step(game, 6,2,5,2);
 
 	set_step(game, 1,7,2,7);
 	set_step(game,7,3,4,0);
 
 	print_details_game(game);
+*/
 
 
-	//upload_Xml(game);
 }
