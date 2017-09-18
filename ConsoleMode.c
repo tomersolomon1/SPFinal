571a5379917f1cd0ab21befba8a3f6bb998a037c
Tomer Solomon <tomer.solomon1@gmail.com>
2017-09-19 00:00:13 +0300

    adding some prints after illegal move / command

--- ConsoleMode.c
+++ ConsoleMode.c
@@ -16,6 +16,15 @@
 #include "MiniMax.h"
 #include "Files.h"
 
+void ask_move(Gameboard *gameboard, bool to_print) {
+	char *colors[] = {"black", "white"};
+	if (to_print) {
+		print_board(gameboard);
+	}
+	printf("%s player - enter your move:\n", colors[gameboard->user_color]);
+	fflush(stdout);
+}
+
 /* presenting the computer's move */
 void present_computer_move(Step *step, Piece *moving_piece, Piece_type promote_to) {
 	char ABC[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
@@ -61,11 +70,9 @@ int begin_game(Gameboard *gameboard, Program_Mode mode) {
 }
 
 bool console_begin_game(Gameboard *gameboard) {
-	char *colors[] = {"black", "white"};
 	bool game_stil_on = begin_game(gameboard, ConsoleMode);
 	if (game_stil_on) {
-		print_board(gameboard);
-		printf("%s player - enter your move:\n", colors[gameboard->user_color]);
+		ask_move(gameboard, true);
 	}
 	return game_stil_on;
 }
@@ -190,18 +197,16 @@ int make_single_move(Gameboard *gameboard, int srow, int scol, int drow, int dco
 	return 2; /* the game is still on */
 }
 
-/* return false if the game is over, otherwise return true */
-bool make_move(Gameboard *gameboard, Command *comm) {
-	char *colors[] = {"black", "white"};
+/* return -1 if illegal move, 0 if the game is over, and 1 otherwise */
+int make_move(Gameboard *gameboard, Command *comm) {
 	if (comm->args_in_range) { /* the move coordinates represent a valid squares on the board */
 		int move_consequences = make_single_move(gameboard, comm->arg1, comm->arg2, comm->arg3, comm->arg4, true, Empty);
 		if (move_consequences == 1) { /* the game is over */
-			return false;
+			return 0;
 		} else if (move_consequences == 2) { /* legal move, and the game is still on */
 			if (gameboard->game_mode == 2) { /* there are two players */
-				print_board(gameboard);
-				printf("%s player - enter your move:\n", colors[gameboard->turn]);
-				return true;
+				ask_move(gameboard, true);
+				return 1;
 			} else { /* it's now the computer turn */
 				Gameboard *copy = copy_board(gameboard);
 				StepValue *best_move = find_best_step(copy, copy->difficulty);
@@ -212,36 +217,36 @@ bool make_move(Gameboard *gameboard, Command *comm) {
 				move_consequences = make_single_move(gameboard, step->srow, step->scol, step->drow, step->dcol, false, best_move->promote_to);
 				destroy_step_value(best_move);
 				if (move_consequences == 1) { /* the game is over */
-					return false;
+					return 0;
 				} else { /* the game is still on */
-					print_board(gameboard);
-					printf("%s player - enter your move:\n", colors[gameboard->turn]);
-					return true;
+					ask_move(gameboard, true);
+					return 1;
 				}
 			}
 		} else { /* illegal move, the user should try again */
-			return true; /* the game is still on */
+			return -1; /* the game is still on */
 		}
 	} else { /* invalid position on the board */
 		printf("Invalid position on the board\n");
-		return true; /* the game is still on */
+		return -1; /* the game is still on */
 	}
 }
 
-bool castling_move(Gameboard *gameboard, Command *comm) {
+/* return -1 if illegal move, 0 if the game is over, and 1 otherwise */
+int castling_move(Gameboard *gameboard, Command *comm) {
 	if (!comm->args_in_range) {
 		printf("Invalid position on the board\n");
-		return true;
+		return -1;
 	} else if (gameboard->board[comm->arg1][comm->arg2]->colur != gameboard->turn) {
 		printf("The specified position does not contain your piece\n");
-		return true;
+		return -1;
 	} else if (gameboard->board[comm->arg1][comm->arg2]->type != Rock) {
 		printf("Wrong position for a rook\n");
-		return true;
+		return -1;
 	} else if (gameboard->board[comm->arg1][comm->arg2]->has_moved
 			|| gameboard->all_pieces[gameboard->turn][KING_INDEX]->has_moved) {
 		printf("Illegal castling move\n"); /* we can't have castling if the rock or the king has moved */
-		return true;
+		return -1;
 	} else {
 		// the rock is fine, we to check the king is in the right place
 		int base_row = gameboard->turn ? WHITE_ROW : BLACK_ROW;
@@ -252,9 +257,20 @@ bool castling_move(Gameboard *gameboard, Command *comm) {
 		comm->arg4 = dcol;
 		if (!is_valid_step(gameboard, comm->arg1, comm->arg2, comm->arg3, comm->arg4)) {
 			printf("Illegal castling move\n"); /* we can't have castling if */
-			return true;
+			return -1;
 		}
 		return make_move(gameboard, comm);
+	}
+}
+
+bool op_handler(Gameboard *gameboard, Command *comm, Op op) {
+	int succeded = (*op)(gameboard, comm);
+	if (succeded == -1) { /* had some problem along the way */
+		ask_move(gameboard, false); /* this type of op's don't print the board in case of error */
+		return true;
+	} else {
+		bool res = (succeded == 1) ? true : false;
+		return res;
 	}
 }
 
@@ -303,27 +319,29 @@ void present_all_moves(Gameboard *gameboard, Piece *piece) {
 	}
 }
 
-void get_moves(Gameboard *gameboard, Command *comm) {
+int get_moves(Gameboard *gameboard, Command *comm) {
 	if (gameboard->game_mode != 1 || (gameboard->difficulty != 1 && gameboard->difficulty != 2)) {
 		printf("ERROR: invalid command\n"); /* get_moves is only supported in game-mode 1, and difficulty levels 1 and 2 */
-		return;
+		return -1;
 	}
 	if (!comm->args_in_range) {
 		printf("Invalid position on the board\n");
-		return;
+		return -1;
 	}
 	if (gameboard->board[comm->arg1][comm->arg2]->colur != gameboard->user_color) {
 		char *colors[] = {"black", "white"};
 		printf("The specified position does not contain %s player piece\n", colors[gameboard->user_color]);
-		return;
+		return -1;
 	}
 	present_all_moves(gameboard, gameboard->board[comm->arg1][comm->arg2]);
+	return 0;
 }
 
 void save_game(Gameboard *gameboard, Command *comm) {
 	FILE *output_file = fopen(comm->file_name, "w");
 	if (output_file == NULL) {
 		printf("File cannot be created or modified\n");
+		ask_move(gameboard, false);
 	} else {
 		save_xml(output_file, gameboard);
 	}
@@ -331,13 +349,16 @@ void save_game(Gameboard *gameboard, Command *comm) {
 	free(comm->file_name);
 }
 
-void undo_move(Gameboard *gameboard) {
+/* return -1 if couldn't undo the move, otherwise return 0 */
+int undo_move(Gameboard *gameboard) {
 	char ABC[]     = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
 	char *colors[] = {"black", "white"};
 	if (gameboard->game_mode == 2) {
 		printf("Undo command not available in 2 players mode\n");
+		return -1;
 	} else if (ArrayListSize(gameboard->history) < 2) { /* no history */
 		printf("Empty history, move cannot be undone\n");
+		return -1;
 	} else {
 		Step *step = ArrayListGetFirst(gameboard->history);
 		printf("Undo move for player %s : <%d,%c> -> <%d,%c>\n",
@@ -347,9 +368,9 @@ void undo_move(Gameboard *gameboard) {
 		printf("Undo move for player %s : <%d,%c> -> <%d,%c>\n",
 				colors[1-gameboard->turn], step->drow+1, ABC[step->scol], step->srow+1, ABC[step->dcol]);
 		undo_step(gameboard, false);
-		print_board(gameboard);
-		printf("%s player - enter your move:\n", colors[gameboard->turn]);
+		ask_move(gameboard, true);
 	}
+	return 0;
 }
 
 /* gameboard is a pointer to a Gameboard object, with default values */
@@ -366,6 +387,9 @@ void manage_console(Gameboard *gameboard) {
 		free(line); /* we don't need the line as a string anymore */
 		if (comm->comm_e == Invalid_command  || (comm->mode != console_mode && comm->mode != Both)) {
 			printf("ERROR: invalid command\n");
+			if (console_mode == GameMode) {
+				ask_move(gameboard, false); /* ask for user's move again in case of illegal command */
+			}
 		} else { /* appropriate command  */
 			switch (comm->comm_e) {
 				case Start:
@@ -379,17 +403,20 @@ void manage_console(Gameboard *gameboard) {
 				case Restore_Default: resore_default_values(gameboard);	break;
 				case Print_Settings:  print_settings(gameboard);	break;
 				case Make_Move:
-					keep_on = make_move(gameboard, comm); /* so we know if the game is over */
+					keep_on = op_handler(gameboard, comm, make_move); /* so we know if the game is over */
 					break;
 				case Castle:
-					keep_on = castling_move(gameboard, comm); /* so we know if the game is over */
+					keep_on = op_handler(gameboard, comm, castling_move); /* so we know if the game is over */
 					break;
-				case Get_Moves: get_moves(gameboard, comm);	break;
+				case Get_Moves: op_handler(gameboard, comm, get_moves); break;
 				case Save: save_game(gameboard, comm); break;
-				case Undo_Move: undo_move(gameboard); break;
+				case Undo_Move:
+					if (undo_move(gameboard) < 0) {
+						ask_move(gameboard, false);
+					}
+						break;
 				case Reset:
 					console_mode = SettingsMode;
-					//reset_game(&gameboard);
 					reset_board(&gameboard);
 					printf("Restarting...\n");
 					printf("Specify game setting or type 'start' to begin a game with the current setting:\n");
--- ConsoleMode.h
+++ ConsoleMode.h
@@ -12,6 +12,8 @@
 #include "Parser.h"
 #define in_range(x) (((x) > -1) && ((x) < (8)))
 
+typedef int (*Op)(Gameboard *gameboard, Command *comm);
+
 extern char *colors[];
 bool load_file(Gameboard **gameboard_p, Command *comm);
 void set_difficulty(Gameboard *gameboard, Command *comm);
@@ -21,5 +23,5 @@ int begin_game(Gameboard *gameboard, Program_Mode mode);
 bool console_begin_game(Gameboard *gameboard);
 int make_single_move(Gameboard *gameboard, int srow, int scol, int drow, int dcol,
 		bool user_turn, Piece_type computer_promotion);
-void get_moves(Gameboard *gameboard, Command *comm);
+int get_moves(Gameboard *gameboard, Command *comm);
 #endif /* CONSOLEMODE_H_ */
